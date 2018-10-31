2d86c0e1b854d856fdd685e6ec2993ac3c7a47fb
Salman <salkhan23@gmail.com>
2018-10-30 21:30:51 -0400

    Use he_normal instead of golrot_normal. Read somewhere it is better

--- models/all_conv_net.py
+++ models/all_conv_net.py
@@ -7,41 +7,41 @@ from keras.layers import Conv2D, Activation, Dropout, GlobalAveragePooling2D, Ba
 def get_model(n_classes, activation='relu'):
     model = Sequential()
 
-    model.add(Conv2D(96, kernel_size=(3, 3), padding='same', input_shape=(32, 32, 3)))
+    model.add(Conv2D(96, kernel_size=(3, 3), padding='same', input_shape=(32, 32, 3), kernel_initializer='he_normal'))
     model.add(Activation(activation))
     model.add(BatchNormalization())
 
-    model.add(Conv2D(96, (3, 3), padding='same'))
+    model.add(Conv2D(96, (3, 3), padding='same', kernel_initializer='he_normal'))
     model.add(Activation(activation))
     model.add(BatchNormalization())
 
-    model.add(Conv2D(96, (3, 3), padding='same', strides=(2, 2)))
+    model.add(Conv2D(96, (3, 3), padding='same', strides=(2, 2), kernel_initializer='he_normal'))
     model.add(BatchNormalization())
     model.add(Dropout(0.5))
 
     # --------------------------------------------
-    model.add(Conv2D(192, (3, 3), padding='same'))
+    model.add(Conv2D(192, (3, 3), padding='same', kernel_initializer='he_normal'))
     model.add(Activation(activation))
     model.add(BatchNormalization())
 
-    model.add(Conv2D(192, (3, 3), padding='same'))
+    model.add(Conv2D(192, (3, 3), padding='same', kernel_initializer='he_normal'))
     model.add(Activation(activation))
     model.add(BatchNormalization())
 
-    model.add(Conv2D(192, (3, 3), padding='same', strides=(2, 2)))
+    model.add(Conv2D(192, (3, 3), padding='same', strides=(2, 2), kernel_initializer='he_normal'))
     model.add(BatchNormalization())
     model.add(Dropout(0.5))
 
     # --------------------------------------------
-    model.add(Conv2D(256, (3, 3), padding='same'))
+    model.add(Conv2D(256, (3, 3), padding='same', kernel_initializer='he_normal'))
     model.add(Activation(activation))
     model.add(BatchNormalization())
 
-    model.add(Conv2D(256, (1, 1), padding='valid'))
+    model.add(Conv2D(256, (1, 1), padding='valid', kernel_initializer='he_normal'))
     model.add(Activation(activation))
     model.add(BatchNormalization())
 
-    model.add(Conv2D(n_classes, (1, 1), padding='valid'))
+    model.add(Conv2D(n_classes, (1, 1), padding='valid', kernel_initializer='he_normal'))
     model.add(BatchNormalization())
 
     model.add(GlobalAveragePooling2D())
