e700f7a61719faca664dd1c0a26ab9321f57879b
Salman <salkhan23@gmail.com>
2018-10-31 21:19:21 -0400

    USe the WideResent model from the keras contrib source

--- models/wide_renset_model.py
+++ models/wide_renset_model.py
@@ -1,23 +1,21 @@
 
 from .wide_residual_network import create_wide_residual_network
+from .wide_resnet import WideResidualNetwork
 import keras.backend as K
 from keras.utils import plot_model
 
+
 def get_model():
-    # For WRN-16-8 put N = 2, k = 8
-    # For WRN-28-10 put N = 4, k = 10
-    # For WRN-40-4 put N = 6, k = 4
+    input_shape = (32, 32, 3)
+    n_classes = 100
 
-    input_shape =(32, 32, 3)
-    nb_classes=100
-
-
-    model = create_wide_residual_network(
-        input_shape,
-        nb_classes=nb_classes,
-        N=4,
-        k=8,
-        dropout=0.0
+    model = WideResidualNetwork(
+        input_shape=input_shape,
+        depth=28,
+        width=8,
+        classes=n_classes,
+        weights=None,
+        dropout_rate=0.0
     )
 
     return model
